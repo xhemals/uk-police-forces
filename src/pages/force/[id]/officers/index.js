47bc74bbc6ec85e094a522945b22391db1281bf7
Jamie Speller <j@miee.uk>
2024-05-03 02:09:21 +0100

    Redesign officer page layout and add rank filter

--- src/pages/force/[id]/officers/index.js
+++ src/pages/force/[id]/officers/index.js
@@ -2,10 +2,13 @@ import { useState } from "react";
 import { SeniorOfficers, SpecificForce } from "@/functions/api-calls";
 import Header from "@/components/layouts/header/header";
 import styles from "./page.module.css";
-import "@/app/globals.css";
-import { PoliceSocials } from "@/functions/police-socials";
-import { networkFor } from "react-social-icons";
+import "@/components/filters/react-select.css";
 import { NextSeo } from "next-seo";
+import Link from "next/link";
+import makeAnimated from "react-select/animated";
+import dynamic from "next/dynamic";
+
+const Select = dynamic(() => import("react-select").then((mod) => mod.default), { ssr: false });
 
 export async function getServerSideProps({ params }) {
 	const id = params.id;
@@ -14,76 +17,71 @@ export async function getServerSideProps({ params }) {
 	return { props: { force, seniorOfficers } };
 }
 
-function socialCheck(url, index) {
-	const lowerUrl = url.toLowerCase();
-	if (url === "") {
-		return;
-	}
-	if (networkFor(url) === "sharethis") {
-		if (lowerUrl.includes("rss") || lowerUrl.includes("xml")) {
-			null;
-		} else {
-			return;
+function getRankFilters(seniorOfficers) {
+	let options = [];
+	seniorOfficers.map((officer) => {
+		if (!options.some((option) => option.value === officer.rank)) {
+			options.push({ value: officer.rank, label: officer.rank });
 		}
-	}
-	return (
-		<div key={index} className={styles.socialLinks}>
-			<a href={url} target="_blank" rel="noreferrer">
-				<PoliceSocials socialUrl={url} />
-			</a>
-		</div>
-	);
+	});
+	return options;
 }
 
 export default function Force({ force, seniorOfficers }) {
-	const [search, setSearch] = useState("");
 	const [visibleOfficers, setVisibleOfficers] = useState(seniorOfficers);
-	if (force.description) {
-		var descriptionWithoutTags = force.description.replace(/<[^>]+>/g, "");
-	}
 
-	if (!force) {
-		return <div>Loading...</div>;
-	}
-
-	const handleInput = (e) => {
-		setSearch(e.target.value);
-		setVisibleOfficers(seniorOfficers.filter((officer) => officer.name.toLowerCase().includes(e.target.value.toLowerCase())));
+	const filterByRank = (selectedOptions) => {
+		if (selectedOptions.length === 0) {
+			setVisibleOfficers(seniorOfficers);
+			return;
+		}
+		setVisibleOfficers(seniorOfficers.filter((officer) => selectedOptions.some((option) => option.value === officer.rank)));
 	};
 
 	return (
 		<>
-			<NextSeo title={`Officers - ${force.name}`} description={descriptionWithoutTags ? descriptionWithoutTags : `Information about ${force.name}`} />
+			<NextSeo title={`Officers - ${force.name}`} description={`Senior officers from ${force.name}`} />
 			<Header />
 			<main className={styles.main}>
-				<h1>{force.name}</h1>
-
-				<a className={styles.link} href={force.url} target="_blank" rel="noreferrer">
-					{force.url}
-				</a>
-				<div className={styles.socials}>
-					{force.engagement_methods.map((method, index) => {
-						return socialCheck(method.url, index);
-					})}
-				</div>
-				{seniorOfficers && seniorOfficers.length > 0 ? (
-					<div className={styles.seniorOfficers}>
-						<h2>Senior Officers</h2>
-						<div className={styles.searchBar}>
-							<input className={styles.search} type="text" value={search} placeholder="Search..." onInput={(e) => handleInput(e)} />
-							<i aria-hidden className={`fa fa-search ${styles.searchIcon}`}></i>
-						</div>
-						<div className={`masonry ${styles.officersDisplay}`}>
-							{visibleOfficers.map((officer, index) => (
-								<div key={index} className={`officer ${styles.officer}`} name={officer.name}>
-									<h3>{officer.name}</h3>
-									<h6>{officer.rank}</h6>
-									{officer.bio ? <div className={styles.bio} dangerouslySetInnerHTML={{ __html: officer.bio }}></div> : null}
+				<h1>
+					<Link href={`/force/${force.id}`}>{force.name}</Link> Senior Officers
+				</h1>
+				<div className={styles.seniorOfficers}>
+					{seniorOfficers && seniorOfficers.length > 0 ? (
+						<>
+							<div className={styles.filterArea}>
+								<div className={styles.rankFilter}>
+									<span>
+										<h3>Rank</h3>
+									</span>
+									<Select
+										blurInputOnSelect
+										isMulti
+										options={getRankFilters(seniorOfficers)}
+										name="Rank"
+										unstyled
+										className="react-select-container"
+										classNamePrefix="react-select"
+										onChange={(selectedOptions) => {
+											filterByRank(selectedOptions);
+										}}
+									/>
 								</div>
-							))}
-						</div>
-					</div>
-				) : null}
+							</div>
+							<div className={`${styles.officersDisplay}`}>
+								{visibleOfficers.map((officer, index) => (
+									<div key={index} className={`officer ${styles.officer}`} name={officer.name}>
+										<h3>{officer.name}</h3>
+										<h6>{officer.rank}</h6>
+										{officer.bio ? <div className={styles.bio} dangerouslySetInnerHTML={{ __html: officer.bio }}></div> : null}
+									</div>
+								))}
+							</div>
+						</>
+					) : (
+						<h4>There are no senior officers listed for {force.name}</h4>
+					)}
+				</div>
 			</main>
 		</>
 	);
