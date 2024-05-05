62236042babdfa3e2094c03e65753db0e91adc6e
Jamie Speller <j@miee.uk>
2024-05-05 05:36:43 +0100

    Refactor API calls and add SpecificSeniorOfficer and Neighbourhoods functions

--- src/functions/api-calls.js
+++ src/functions/api-calls.js
@@ -15,20 +15,13 @@ export const SeniorOfficers = async (id) => {
 	return response.data;
 };
 
-export const SpecificSeniorOfficer = async (id) => {};
-
-export const SeniorOfficerPreview = async (id) => {
+export const SpecificSeniorOfficer = async (id, name) => {
 	const response = await axios.get(`https://data.police.uk/api/forces/${id}/people`);
-	const officers = response.data;
-	if (officers.length < 3) {
-		return officers;
-	}
-	let randomOfficers = [];
-	while (randomOfficers.length < 3) {
-		let randomIndex = Math.floor(Math.random() * officers.length);
-		if (randomOfficers.indexOf(officers[randomIndex]) === -1) {
-			randomOfficers.push(officers[randomIndex]);
-		}
-	}
-	return randomOfficers;
+	const officerInfo = response.data.filter((officer) => officer.name === name);
+	return officerInfo[0];
+};
+
+export const Neighbourhoods = async (id) => {
+	const response = await axios.get(`https://data.police.uk/api/${id}/neighbourhoods`);
+	return response.data;
 };
