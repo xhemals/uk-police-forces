b3019728e8e24ab18701d798fb5a68b7b73585c3
Jamie Speller <j@miee.uk>
2024-05-05 05:35:51 +0100

    Add Header component to _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,4 @@
-// pages/_document.js
+import Header from "@/components/layouts/header/header";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
@@ -9,6 +9,7 @@ class MyDocument extends Document {
 					<script async src="https://kit.fontawesome.com/c4b491e14f.js" crossOrigin="anonymous"></script>
 				</Head>
 				<body>
+					<Header />
 					<Main />
 					<NextScript />
 				</body>
