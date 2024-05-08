8cba7b627270d2e9579c5499a935f84556af87f7
Jamie Speller <j@miee.uk>
2024-05-08 02:23:14 +0100

    Refactor imports and sanitize officer's bio

--- src/pages/force/[id]/officers/[officer].js
+++ src/pages/force/[id]/officers/[officer].js
@@ -4,6 +4,9 @@ import Link from "next/link";
 import dynamic from "next/dynamic";
 import { NextSeo } from "next-seo";
 
+//other imports
+import DOMPurify from "isomorphic-dompurify";
+
 // Component imports
 import Header from "@/components/layouts/header/header";
 
@@ -11,7 +14,8 @@ import Header from "@/components/layouts/header/header";
 import styles from "./page.module.css";
 
 // Function imports
-import { SpecificForce, SpecificSeniorOfficer } from "@/functions/api-calls";
+import { SpecificForce, SpecificSeniorOfficer } from "@/functions/data-calls";
+import RenderHTML from "@/functions/render-html";
 
 export async function getServerSideProps({ params }) {
 	const { id, officer } = params;
@@ -22,20 +26,22 @@ export async function getServerSideProps({ params }) {
 		.join(" ");
 	const officerInfo = await SpecificSeniorOfficer(id, officerName);
 	const force = await SpecificForce(id);
-	// const force = await SpecificForce(id);
-	// const seniorOfficers = await SeniorOfficers(id);
 	return { props: { officerName, officerInfo, force } };
 }
 
 export default function Force({ officerName, officerInfo, force }) {
+	const cleanBio = DOMPurify.sanitize(officerInfo.bio);
 	return (
 		<>
-			<NextSeo title={`${officerName} - ${force.name}`} description={officerInfo.bio ? officerInfo.bio : `Information about ${officerName} from ${force.name}`} />
-			{/* <Header /> */}
+			<NextSeo title={`${officerName} - ${force.name}`} description={officerInfo.bio ? officerInfo.bio.replace(/<[^>]*>/g, "") : `Information about ${officerName} from ${force.name}`} />
 			<main className={styles.main}>
 				<h1>{officerName}</h1>
-				<h2>{officerInfo.bio}</h2>
-				<h3>{force.name}</h3>
+				<h3>
+					<Link href={`/force/${force.id}`}>{force.name}</Link>
+				</h3>
+				<div>
+					<RenderHTML html={officerInfo.bio} />
+				</div>
 			</main>
 		</>
 	);
