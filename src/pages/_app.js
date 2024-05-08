ecc0a204909dac717b55b1ee5e7c52335de8cd3a
Jamie Speller <j@miee.uk>
2024-05-08 02:20:42 +0100

    Add header and footer components to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,26 @@
 import React from "react";
 import RootLayout from "@/app/layout";
+import "@/components/layouts/header/header.css";
+import "@/components/layouts/footer/footer.css";
+import Footer from "@/components/layouts/footer/footer";
+import { DbLastUpdated } from "@/functions/data-calls";
+import Header from "@/components/layouts/header/header";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, lastUpdated }) {
 	return (
 		<>
+			<Header />
 			<RootLayout>
 				<Component {...pageProps} />
 			</RootLayout>
+			<Footer lastUpdated={lastUpdated} />
 		</>
 	);
 }
+
+MyApp.getInitialProps = async () => {
+	const result = await DbLastUpdated();
+	return { lastUpdated: result.date };
+};
 
 export default MyApp;
