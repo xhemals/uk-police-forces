93caec363de26f0460cd4fc2412ceacd39170954
Jamie Speller <j@miee.uk>
2024-05-08 02:18:29 +0100

    Remove unused CSS import and commented code in header.js

--- src/components/layouts/header/header.js
+++ src/components/layouts/header/header.js
@@ -1,12 +1,4 @@
-// const headerStyle = {
-// 	backgroundColor: "blue",
-// 	color: "white",
-// 	width: "100%",
-// 	height: "50px",
-// };
-
 import Link from "next/link";
-import "./header.css";
 
 const Header = () => (
 	<header className="Header">
