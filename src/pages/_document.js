f44edfe09ef0ed61d255dac4687c0e09c5ba7a88
Jamie Speller <j@miee.uk>
2024-05-08 02:21:06 +0100

    Remove Header component from _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,3 @@
-import Header from "@/components/layouts/header/header";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
@@ -9,7 +8,6 @@ class MyDocument extends Document {
 					<script async src="https://kit.fontawesome.com/c4b491e14f.js" crossOrigin="anonymous"></script>
 				</Head>
 				<body>
-					<Header />
 					<Main />
 					<NextScript />
 				</body>
