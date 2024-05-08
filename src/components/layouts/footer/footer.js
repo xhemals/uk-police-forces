d39c5a2e21c77aea72221d078d584f4a3046caf7
Jamie Speller <j@miee.uk>
2024-05-08 02:18:15 +0100

    Add footer component with CSS and JS files

---
+++ src/components/layouts/footer/footer.js
@@ -0,0 +1,24 @@
+import { DbLastUpdated } from "@/functions/data-calls";
+import Link from "next/link";
+
+export default function Footer({ lastUpdated }) {
+	const currentYear = new Date().getFullYear();
+	const lastUpdatedFormatted = `${new Date(lastUpdated).toLocaleString("default", { month: "long" })} ${new Date(lastUpdated).getFullYear()}`;
+	return (
+		<footer className="footer">
+			<div className="row-1">
+				<div className="footer-copyright">
+					&copy; {currentYear} <Link href="https://github.com/xhemals">xhemals</Link>
+				</div>
+				<div className="last-updated">Last updated {lastUpdatedFormatted}</div>
+			</div>
+			<div className="row-2">
+				<div class="disclaimer">
+					<p>
+						Disclaimer: This website is an independent service and is not affiliated with, endorsed by, or in any way associated with the UK Police Force. <Link href="/disclaimer">Read more.</Link>
+					</p>
+				</div>
+			</div>
+		</footer>
+	);
+}
