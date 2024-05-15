6106650c8bd8d54c558f4cac8741342e4c42cadc
Jamie Speller <j@miee.uk>
2024-05-16 00:06:57 +0100

    Add SEO and analytics components to _app.js and update favicon and manifest in _document.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,10 +5,23 @@ import "@/components/layouts/footer/footer.css";
 import Footer from "@/components/layouts/footer/footer";
 import { DbLastUpdated } from "@/functions/data-calls";
 import Header from "@/components/layouts/header/header";
+import { Analytics } from "@vercel/analytics/react";
+import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps, lastUpdated }) {
 	return (
 		<>
+			<DefaultSeo
+				openGraph={{
+					images: [
+						{
+							url: "/api/og",
+							alt: "UK Police Force Information Logo",
+						},
+					],
+				}}
+			/>
+			<Analytics />
 			<Header />
 			<RootLayout>
 				<Component {...pageProps} />
