a2572a4a9d65cc09f3395a970827ac236cb3b4d8
Jamie Speller <j@miee.uk>
2024-05-16 00:06:16 +0100

    Update header layout and add logo image

--- src/components/layouts/header/header.js
+++ src/components/layouts/header/header.js
@@ -1,8 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Header = () => (
 	<header className="Header">
-		<Link href="/">Home</Link>
+		<Link href="/">
+			<Image src="/logo-full-removebg.png" width="322" height="84" />
+		</Link>
 	</header>
 );
 
