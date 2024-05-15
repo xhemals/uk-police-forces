8e9ef7c3f0d7455c8967855b78724cdf87140344
Jamie Speller <j@miee.uk>
2024-05-16 00:13:12 +0100

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,12 +1,19 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-	siteUrl: process.env.website || "https://example.com",
-	generateRobotsTxt: true, // (optional)
+	siteUrl: process.env.website,
+	generateRobotsTxt: true,
 	exclude: ["/api/*", "/server-sitemap.xml"],
 	sitemapSize: 500,
 	robotsTxtOptions: {
 		additionalSitemaps: [
 			`${process.env.website}/server-sitemap.xml`, // <==== Add here
+		],
+		policies: [
+			{
+				userAgent: "*",
+				allow: "/",
+				disallow: "/api",
+			},
 		],
 	},
 };
